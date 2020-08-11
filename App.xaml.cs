723864e058ea59ff9d99a9e5588351ab87d7325b
Sergio Velmay <sergiovelmay@gmail.com>
2020-08-11 09:00:54 +0200

    Solution and project created from VS.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace YWIL_YouWorkItLooks
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
