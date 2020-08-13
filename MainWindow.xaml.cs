83aa0b64fbc5a56650ccf3f2472617b26f2ae096
Sergio Velmay <sergiovelmay@gmail.com>
2020-08-13 09:59:32 +0200

    Prediction workflow created.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.Threading;
@@ -9,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 using Intel.RealSense;
+using Newtonsoft.Json;
 
 namespace YWIL_YouWorkItLooks
 {
@@ -22,19 +24,58 @@ namespace YWIL_YouWorkItLooks
 
         private int frameCount = 0;
 
-        private int imageEach = 30;
+        private int imageEach = 15;
 
         private Int32Rect rect = new Int32Rect(0, 0, 1280, 720);
 
-        private int left = (1280 - 720) / 2;
+        private int cropLeft = (1280 - 720) / 2;
 
         private Int32Rect region = new Int32Rect((1280 - 720) / 2, 0, 720, 720);
 
         private double scale = 416d / 720d;
 
-        private const bool capturingFlag = true;
+        private const bool capturingFlag = false;
 
-        private string lastFolder = "C_ClassificationMultiClass";
+        private List<string> modelFolders = new List<string>()
+        {
+            "ClassificationMultilabel",
+            "ObjectDetection",
+            "ClassificationMulticlass"
+        };
+
+        private List<string> multilabelLabels = new List<string>()
+        {
+            "Step0", 
+            "Step1", 
+            "Step2", 
+            "Step3", 
+            "true", 
+            "false", 
+            "Step4", 
+            "front", 
+            "back"
+        };
+
+        private List<string> objectDetectionLabels = new List<string>()
+        {
+            "Step5.hole",
+            "Step5.part",
+            "Step5.error",
+            "Step6.hole",
+            "Step6.part"
+        };
+
+        private List<string> multiclassLabels = new List<string>()
+        {
+            "Step7.true",
+            "Step7.false.A",
+            "Step7.false.B"
+        };
+
+        private int currentModel = 0; // 0 --> 01234 / 1 --> 56 / 2 --> 7
+
+        private int currentStep = 0;
+
         private string lastFramePath;
 
         private Action<VideoFrame> UpdateImage(Image image)
@@ -49,42 +90,165 @@ namespace YWIL_YouWorkItLooks
 
                 if (frameCount % imageEach == 0)
                 {
-                    ProcessBitmapFrame(bitmap);
+                    _ = ProcessBitmapFrame(bitmap);
                 }
             });
         }
 
-        private void ProcessBitmapFrame(BitmapSource bitmap)
+        private async Task ProcessBitmapFrame(BitmapSource bitmap)
         {
-            if (capturingFlag)
+            BitmapSource cropped = new CroppedBitmap(bitmap, region);
+
+            TransformedBitmap scaled = new TransformedBitmap(cropped, new ScaleTransform(scale, scale));
+
+            System.IO.Stream stream = StreamFromBitmapSource(scaled);
+
+            SaveStreamAsPngFile(stream, capturingFlag);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            string result = await ModelInference(modelFolders[currentModel]);
+
+            stopwatch.Stop();
+            int inferenceTime = (int)stopwatch.ElapsedMilliseconds;
+
+            switch (currentModel)
             {
-                BitmapSource cropped = new CroppedBitmap(bitmap, region);
+                case 0:
+                    List<Classification> multilabel = JsonConvert.DeserializeObject<List<Classification>>(result);
+                    ShowHelpMessage(multilabel, inferenceTime);
+                    break;
+                case 1:
+                    List<Detection> objectDetection = JsonConvert.DeserializeObject<List<Detection>>(result);
+                    ShowHelpMessage(objectDetection, inferenceTime);
+                    ShowAllDetectionBoxes(objectDetection);
+                    break;
+                case 2:
+                    List<Classification> multiclass = JsonConvert.DeserializeObject<List<Classification>>(result);
+                    ShowHelpMessage(multiclass, inferenceTime);
+                    break;
+                default:
+                    break;
+            }
+        }
 
-                TransformedBitmap scaled = new TransformedBitmap(cropped, new ScaleTransform(scale, scale));
+        private void ShowHelpMessage<T>(List<T> items, int milliseconds) where T : Classification
+        {
+            string message = "Time: " + milliseconds + ". ";
 
-                System.IO.Stream stream = StreamFromBitmapSource(scaled);
+            foreach (T item in items)
+            {
+                message += " " + item.PredictionToString() + " ";
+            }
 
-                SaveStreamAsPngFile(stream);
+            helpMessageTextBlock.Text = message;
+        }
 
+        private void ClearDetectionBoxes()
+        {
+            objectDetectionCanvas.Children.Clear();
+        }
 
-                string workingDir = @$"{System.IO.Directory.GetCurrentDirectory()}\{lastFolder}";
-                ProcessStartInfo startInfo = new ProcessStartInfo();
-                startInfo.WorkingDirectory = workingDir;
-                startInfo.FileName = "cmd.exe";
-                startInfo.Arguments = $"/C py script.py -i {lastFramePath}";
-                startInfo.UseShellExecute = false;
-                startInfo.RedirectStandardOutput = true;
-                startInfo.CreateNoWindow = true;
+        private void ShowAllDetectionBoxes(IEnumerable<Detection> detectedObjects)
+        {
+            ClearDetectionBoxes();
 
-                using (Process process = Process.Start(startInfo))
+            foreach (Detection detection in detectedObjects)
+            {
+                ShowObjectDetectionBox(detection);
+            }
+        }
+
+        private void ShowObjectDetectionBox(Detection detectedObject)
+        {
+            double canvasSide = 720;
+
+            float probability = float.Parse(detectedObject.Probability);
+
+            Color color;
+
+            //if (detectedObject.TagName.Contains("_MAL"))
+            //{
+            //    color = Colors.Red;
+            //}
+            //else
+            //{
+            if (probability > 0.90)
+                color = Colors.Lime;
+            else if (probability > 0.80)
+                color = Colors.LawnGreen;
+            else if (probability > 0.70)
+                color = Colors.Yellow;
+            else if (probability > 0.60)
+                color = Colors.Orange;
+            else
+                color = Colors.Red;
+            //}
+
+            objectDetectionCanvas.Children.Add(
+                new Border
                 {
-                    using (System.IO.StreamReader reader = process.StandardOutput)
-                    {
-                        string result = reader.ReadToEnd();
-                        Console.Write(result);
-                    }
+                    BorderBrush = new SolidColorBrush(color),
+                    BorderThickness = new Thickness(2),
+                    Margin = new Thickness(detectedObject.Box.Left * canvasSide + cropLeft,
+                                            detectedObject.Box.Top * canvasSide, 0, 0),
+                    Width = detectedObject.Box.Width * canvasSide,
+                    Height = detectedObject.Box.Height * canvasSide,
+                }
+            );
+
+            //objectDetectionCanvas.Children.Add(
+            //    new Border
+            //    {
+            //        Height = 27,
+            //        FlowDirection = FlowDirection.LeftToRight,
+            //        HorizontalAlignment = HorizontalAlignment.Left,
+            //        Margin = new Thickness(detectedObject.Box.Left * canvasSide + cropLeft,
+            //                                detectedObject.Box.Top * canvasSide - 25, 0, 0),
+
+            //        Child = new Border
+            //        {
+            //            Background = new SolidColorBrush(color),
+            //            HorizontalAlignment = HorizontalAlignment.Left,
+            //            VerticalAlignment = VerticalAlignment.Bottom,
+            //            Child =
+            //                new TextBlock
+            //                {
+            //                    Foreground = new SolidColorBrush(Colors.Black),
+            //                    Text = $"{detectedObject.Label} {detectedObject.Probability}%",
+            //                    FontSize = 9,
+            //                    FontWeight = FontWeight.FromOpenTypeWeight(500),
+            //                    Margin = new Thickness(8, 2, 8, 2)
+            //                }
+            //        }
+            //    }
+            //);
+        }
+
+        private async Task<string> ModelInference(string modelFolder)
+        {
+            string workingDir = @$"{System.IO.Directory.GetCurrentDirectory()}\{modelFolder}";
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WorkingDirectory = workingDir;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = $"/C py script.py -i {lastFramePath}";
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.CreateNoWindow = true;
+
+            string result = string.Empty;
+
+            using (Process process = Process.Start(startInfo))
+            {
+                using (System.IO.StreamReader reader = process.StandardOutput)
+                {
+                    result = await reader.ReadToEndAsync();
                 }
             }
+
+            return result;
         }
 
         private System.IO.Stream StreamFromBitmapSource(BitmapSource bitmap)
@@ -102,11 +266,18 @@ namespace YWIL_YouWorkItLooks
             return stream;
         }
 
-        private void SaveStreamAsPngFile(System.IO.Stream inputStream)
+        private void SaveStreamAsPngFile(System.IO.Stream inputStream, bool capture)
         {
-            string dateTime = DateTime.Now.ToString("yyyyMMdd_HHmmss_fff", CultureInfo.CurrentUICulture.DateTimeFormat);
+            string dateName = string.Empty;
 
-            string fileName = "YWIL_YouWorkItLooks_" + dateTime + ".png";
+            if (capture)
+            {
+                string dateTime = DateTime.Now.ToString("yyyyMMdd_HHmmss_fff", CultureInfo.CurrentUICulture.DateTimeFormat);
+
+                dateName += "_" + dateTime;
+            }
+
+            string fileName = "YWIL_YouWorkItLooks" + dateName + ".png";
 
             string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\YWIL_YouWorkItLooks";
 
@@ -223,10 +394,10 @@ namespace YWIL_YouWorkItLooks
         {
             using (VideoStreamProfile stream = pipelineProfile.GetStream(Stream.Color).As<VideoStreamProfile>())
             {
-                colorImage.Source = new WriteableBitmap(stream.Width, stream.Height, 96d, 96d, PixelFormats.Rgb24, null);
+                colorRealSenseImage.Source = new WriteableBitmap(stream.Width, stream.Height, 96d, 96d, PixelFormats.Rgb24, null);
             }
 
-            color = UpdateImage(colorImage);
+            color = UpdateImage(colorRealSenseImage);
         }
     }
 }
