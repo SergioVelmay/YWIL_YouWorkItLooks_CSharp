2d7706306fa258516c15308676a54f6e7ad9ae45
Sergio Velmay <sergiovelmay@gmail.com>
2020-08-31 00:29:09 +0200

    Multilabel help messages added.

--- Steps/MultilabelSteps.cs
+++ Steps/MultilabelSteps.cs
@@ -19,10 +19,13 @@ namespace YWIL_YouWorkItLooks.Steps
 
         public static readonly List<string> MultilabelHelpMessages = new List<string>()
         {
+            "Multilabel classification from 0 to 4 ready",
+            "Waiting for the white base to be placed...",
             "Place the first part in the rectangular hole",
             "Place the red part as shown in the help image",
-            "Place the pink part with the round facing up",
-            "Place the gray rotor on top of the pink part"
+            "Place the pink part with the hole facing up",
+            "Place the gray rotor on top of the pink part",
+            "Well done! Let's continue with the next steps"
         };
 
         public static readonly List<string> MultilabelErrorMessages = new List<string>()
