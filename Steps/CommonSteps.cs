8ad4261c5907e24719dabaa1610a2b1ce1665e6b
Sergio Velmay <sergiovelmay@gmail.com>
2020-09-11 07:59:11 +0200

    New image resource URIs added.

--- Steps/CommonSteps.cs
+++ Steps/CommonSteps.cs
@@ -16,14 +16,16 @@ namespace YWIL_YouWorkItLooks.Steps
         {
             "#ec1f24", // Red --> No
             "#0ea84b", // Green --> Yes
-            "#868686" // Gray --> Arrow
+            "#868686" // Gray --> Pause - Arrow
         };
 
         public static readonly List<Uri> StepCheckingUris = new List<Uri>()
         {
             new Uri("pack://application:,,,/Resources/StepNo.png"),
             new Uri("pack://application:,,,/Resources/StepYes.png"),
-            new Uri("pack://application:,,,/Resources/StepArrow.png")
+            new Uri("pack://application:,,,/Resources/StepPause.png"),
+            new Uri("pack://application:,,,/Resources/StepArrow.png"),
+            new Uri("pack://application:,,,/Resources/AssemblyCompleted.png")
         };
 
         public static Uri GetImageStepUri(int step)
